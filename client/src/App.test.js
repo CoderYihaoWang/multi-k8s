4dda4ddf5039c42351ff65b6910bb0ca3112b624
CoderYihaoWang <yesiamyihao@gmail.com>
2021-01-14 00:23:43 +1300

    Try to fix travis ci run test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,4 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Fibonacci Calculator/i);
+  expect(linkElement).toBeInTheDocument();
 });
